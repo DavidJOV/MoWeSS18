61fbbd58d704fe1dcd8290b6855a7e506a8f951b
paulovpinto <33468773+paulovpinto@users.noreply.github.com>
2018-08-26 22:03:14 +0200

    Add files via upload
    Dokumentation und Index.php für SEO aufgabe uploaded

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="publisher" content="Technische Hochschule Köln, Campus Gummersbach">
+	<meta name="author" content="Andre Kasper">
+	<meta name="description" content="SEO Testdatei">
+	<!-- keywords added -->
+	<meta name="keywords" content="suchmaschinenoptimierung, linkbuilding, ranking faktoren, online marketing, optimierung, campixx, suchmaschinen, marketing, analyse, keyword, tool, ranking,offpage optimierung, keywords" />
+	<meta name="date" content="<?=date('c', getlastmod())?>">
+	<link rel="stylesheet" href="css/datatables.min.css" type="text/css">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<header>
+		<img src="img/logo.jpg">
+	</header>
+	<main>
+		<div id="mydiv">
+			<h2>Super coole Seite!</h2>
+			<p>
+				Dies ist eine super coole Seite! Nur leider nicht SEO-optimiert :( Könnt ihr mir helfen?
+			</p>
+			<!-- <p>Hier ein Link: <a href="seo.php">Klick mich!</a></p> --> <!--Dead Link removed -->
+		</div>
+		<div id="mydiv">
+			<h3>WPF MoWE</h3>
+			<p>
+				Für das WPF Moderner Webentwicklung mit HTML5, CSS3 und JavaScript sollen 3 mögliche Optimierungen für diese Webseite gefunden und in der Dokumentation aufgeschrieben werden!
+			</p>
+			<p>
+				Viel Spaß und viel Erfolg!<br>
+				Andre Kasper
+			</p>
+		</div>
+			<a href="http://th-koeln.de">TH Köln Website </a> <!-- iFrame removed -->
+	</main>
+	<footer>
+		&copy; 2017 Andre Kasper
+	</footer>
+</body>
+</html>
